10906e521464be095532281d0912d0cb8dc61ac0
DrgnFireYellow <ethanmilbert@gmail.com>
2024-01-16 18:29:00 -0500

    Improve display names of emoji

--- full/My Emoji.py
+++ full/My Emoji.py
@@ -6,5 +6,5 @@ streamlit.write("# My Emoji")
 
 for emoji in os.listdir("emojis"):
     if not emoji == ".gitkeep":
-        streamlit.write(os.path.splitext(emoji)[0])
+        streamlit.write(os.path.splitext(emoji)[0].replace("_", " ").replace("-", " "))
         streamlit.image(os.path.join("emojis", emoji))
